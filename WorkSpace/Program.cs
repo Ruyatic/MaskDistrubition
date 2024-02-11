f7592063e6344e2db95bd5ba9f0f2da9c3bb4e1c
ruyat <ruyat@RUYAKI>
2024-02-11 20:30:48 +0300

    Add project files.

---
+++ WorkSpace/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using Business.Concrete;
+using Entities.Concrete;
+
+Person person1 = new Person();
+person1.FirstName = "A";
+person1.LastName = "B";
+person1.NationalIdentity = 123;
+person1.DateOfBirthYear = 2000;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
